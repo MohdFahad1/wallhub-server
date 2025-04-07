79ec2107eaaeaeacba165b8c005c87deae67a32e
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2025-04-07 22:51:53 +0530

    feat: Added delete category api

--- src/controllers/category.controllers.js
+++ src/controllers/category.controllers.js
@@ -55,7 +55,38 @@ const getAllCategories = async (req, res) => {
   }
 };
 
+const deleteCategory = async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (!id) {
+      res.status(400).json({
+        success: false,
+        message: "Please provide category id",
+      });
+    }
+    const category = await Category.findByIdAndDelete(id);
+    if (!category) {
+      return res.status(404).json({
+        success: false,
+        message: "Category not found",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Category deleted successfully",
+      category,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+
 module.exports = {
   addCategory,
   getAllCategories,
+  deleteCategory,
 };
--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   addCategory,
   getAllCategories,
+  deleteCategory,
 } = require("../controllers/category.controllers.js");
 
 router.post("/add", addCategory);
 router.get("/", getAllCategories);
+router.delete("/delete/:id", deleteCategory);
 
 module.exports = router;
